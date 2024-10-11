e953e47a731479edb0c41a94975b40b2905d266d
reyhane <97376622+reyhane1376@users.noreply.github.com>
2024-10-11 13:11:28 +0330

    add breeze

--- database/migrations/2024_10_11_082710_create_products_table.php
+++ database/migrations/2024_10_11_082710_create_products_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
+            $table->string('slug', '100');
+            $table->string('title', '150');
             $table->integer('price');
             $table->timestamps();
         });
